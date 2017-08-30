a23cbd9176ac1aec1d7edd096f6af153919f84cf
ziahaqi <zia.ulhack@gmail.com>
2017-08-30 13:13:51 +0700

    update sdk config

--- app/src/main/java/midtrans/mysamplesdk/MainActivity.java
+++ app/src/main/java/midtrans/mysamplesdk/MainActivity.java
@@ -2,12 +2,15 @@ package midtrans.mysamplesdk;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.midtrans.sdk.corekit.callback.TransactionFinishedCallback;
 import com.midtrans.sdk.corekit.core.MidtransSDK;
+import com.midtrans.sdk.corekit.core.PaymentMethod;
 import com.midtrans.sdk.corekit.core.TransactionRequest;
 import com.midtrans.sdk.corekit.core.themes.CustomColorTheme;
-import com.midtrans.sdk.corekit.models.BillInfoModel;
 import com.midtrans.sdk.corekit.models.CustomerDetails;
 import com.midtrans.sdk.corekit.models.ItemDetails;
 import com.midtrans.sdk.corekit.models.snap.CreditCard;
@@ -17,29 +20,54 @@ import com.midtrans.sdk.uikit.SdkUIFlowBuilder;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements TransactionFinishedCallback {
+    private Button buttonUiKit, buttonDirectCreditCard, buttonDirectBcaVa, buttonDirectMandiriVa,
+            buttonDirectBniVa, buttonDirectAtmBersamaVa, buttonDirectPermataVa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindViews();
         // SDK initiation for UIflow
         initMidtransSdk();
+    }
 
+    private void bindViews() {
+        buttonUiKit = (Button) findViewById(R.id.button_uikit);
+        buttonDirectBcaVa = (Button) findViewById(R.id.button_direct_bca_va);
+        buttonDirectMandiriVa = (Button) findViewById(R.id.button_direct_mandiri_va);
+        buttonDirectBniVa = (Button) findViewById(R.id.button_direct_bni_va);
+        buttonDirectPermataVa = (Button) findViewById(R.id.button_direct_permata_va);
+        buttonDirectAtmBersamaVa = (Button) findViewById(R.id.button_direct_atm_bersama_va);
+
+        buttonUiKit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MidtransSDK.getInstance().setTransactionRequest(initTransactionRequest());
+                MidtransSDK.getInstance().startPaymentUiFlow(MainActivity.this);
+            }
+        });
+
+        buttonDirectCreditCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MidtransSDK.getInstance().setTransactionRequest(initTransactionRequest());
+                MidtransSDK.getInstance().startPaymentUiFlow(MainActivity.this, PaymentMethod.CREDIT_CARD);
+            }
+        });
+    }
+
+    private TransactionRequest initTransactionRequest() {
         // Create new Transaction Request
         TransactionRequest transactionRequestNew = new
                 TransactionRequest(System.currentTimeMillis() + "", 6000);
 
-        //define customer detail (mandatory for coreflow)
-        CustomerDetails mCustomerDetails = new CustomerDetails();
-        mCustomerDetails.setPhone("624234234234");
-
-        mCustomerDetails.setFirstName("sample full name");
-
-        mCustomerDetails.setEmail("mail@mail.com");
-        transactionRequestNew.setCustomerDetails(mCustomerDetails);
+        //set customer details
+        transactionRequestNew.setCustomerDetails(initCustomerDetails());
 
 
-        // Define item details
+        // set item details
         ItemDetails itemDetails = new ItemDetails("1", 1000, 1, "Trekking Shoes");
         ItemDetails itemDetails1 = new ItemDetails("2", 1000, 2, "Casual Shoes");
         ItemDetails itemDetails2 = new ItemDetails("3", 1000, 3, "Formal Shoes");
@@ -50,24 +78,31 @@ public class MainActivity extends AppCompatActivity implements TransactionFinish
         itemDetailsArrayList.add(itemDetails1);
         itemDetailsArrayList.add(itemDetails2);
         transactionRequestNew.setItemDetails(itemDetailsArrayList);
-        // Set Bill info
-        BillInfoModel billInfoModel = new BillInfoModel("demo_label", "demo_value");
-        transactionRequestNew.setBillInfoModel(billInfoModel);
+
 
         // Create creditcard options for payment
-        // noted : channel migs is needed if bank type is BCA, BRI or MyBank
         CreditCard creditCard = new CreditCard();
 
-        String cardClickType = getString(R.string.card_click_type_two_click);
-        creditCard.setSaveCard(true);
-        creditCard.setSecure(false);
+        creditCard.setSaveCard(true); // when using one/two click set to true and if normal set to  false
+        creditCard.setSecure(false); // when using one click must be true, for normal and two click (optional)
+
+        // noted !! : channel migs is needed if bank type is BCA, BRI or MyBank
+//        creditCard.setChannel(CreditCard.MIGS); //set channel migs
+//        creditCard.setChannel(BankType.BCA); //set spesific acquiring bank
+
         transactionRequestNew.setCreditCard(creditCard);
 
+        return transactionRequestNew;
+    }
 
-        transactionRequestNew.setCardPaymentInfo(cardClickType, true);
+    private CustomerDetails initCustomerDetails() {
 
-        MidtransSDK.getInstance().setTransactionRequest(transactionRequestNew);
-        MidtransSDK.getInstance().startPaymentUiFlow(this);
+        //define customer detail (mandatory for coreflow)
+        CustomerDetails mCustomerDetails = new CustomerDetails();
+        mCustomerDetails.setPhone("085310102020");
+        mCustomerDetails.setFirstName("user fullname");
+        mCustomerDetails.setEmail("mail@mail.com");
+        return mCustomerDetails;
     }
 
     private void initMidtransSdk() {
@@ -75,14 +110,14 @@ public class MainActivity extends AppCompatActivity implements TransactionFinish
         String base_url = SdkConfig.MERCHANT_BASE_CHECKOUT_URL;
 
         SdkUIFlowBuilder.init(this, client_key, base_url, this)
-                .enableLog(true)
-                .setColorTheme(new CustomColorTheme("#FFE51255", "#B61548", "#FFE51255"))
-                .useBuiltInTokenStorage(false)
+                .enableLog(true) // enable sdk log
+                .setColorTheme(new CustomColorTheme("#FFE51255", "#B61548", "#FFE51255")) // will replace theme on snap theme on MAP
+                .useBuiltInTokenStorage(true)  // set to false if you want to your own token storage (just for two click)
                 .buildSDK();
     }
 
     @Override
     public void onTransactionFinished(TransactionResult transactionResult) {
-
+        Toast.makeText(this, "sdk callback", Toast.LENGTH_SHORT).show();
     }
 }
